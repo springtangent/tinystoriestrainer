42bc93856c3d156b4cdcdf5aef13d92db36bd51b
Ben Frech <b.a.frech@gmail.com>
2023-11-10 00:00:38 -0600

    added en wikipedia dataset.

--- wikipedia_prepare.py
+++ wikipedia_prepare.py
@@ -6,26 +6,27 @@ from settings import END_OF_TEXT, MAX_LENGTH, tokenizer
 # from apache_beam import DirectRunner
 
 
-# Load the Wikipedia Simple dataset
-dataset = load_dataset('wikipedia', '20220301.simple', beam_runner="DirectRunner")
 
-# Define the chunk size
-chunk_size = MAX_LENGTH
+def process_wikipedia_dataset(dataset_split, output_name):
+    # Load the Wikipedia Simple dataset
+    dataset = load_dataset('wikipedia', '20220301.simple', beam_runner="DirectRunner")
 
-# Function to tokenize and chunk the text
-def tokenize_and_chunk(example):
-    # Tokenize the text
-    tokenized_text = tokenizer(example['text'], return_attention_mask=False)
+    # Function to tokenize and chunk the text
+    def tokenize_and_chunk(example):
+        # Tokenize the text
+        tokenized_text = tokenizer(example['text'], return_attention_mask=False)
 
-    # Chunk tokenized text into segments of `chunk_size` tokens
-    chunked_input_ids = [tokenized_text['input_ids'][i:i + chunk_size] for i in range(0, len(tokenized_text['input_ids']), chunk_size)]
+        # Chunk tokenized text into segments of `chunk_size` tokens
+        chunked_input_ids = [tokenized_text['input_ids'][i:i + MAX_LENGTH] for i in range(0, len(tokenized_text['input_ids']), MAX_LENGTH)]
 
-    return {'input_ids': chunked_input_ids}
+        return {'input_ids': chunked_input_ids}
 
-# Tokenize and chunk the dataset
-dataset = dataset.map(tokenize_and_chunk, batched=True, remove_columns=dataset['train'].column_names)
-dataset = dataset['train'].train_test_split(test_size=0.05)
+    # Tokenize and chunk the dataset
+    dataset = dataset.map(tokenize_and_chunk, batched=True, remove_columns=dataset['train'].column_names)
+    dataset = dataset['train'].train_test_split(test_size=0.05)
 
-dataset.save_to_disk("prepared_wikipedia_simple")
+    dataset.save_to_disk("prepared_wikipedia_simple")
 
-
+if __name__ == '__main__':
+    process_wikipedia_dataset('20220301.simple', "prepared_wikipedia_simple")
+    process_wikipedia_dataset('20220301.en', "prepared_wikipedia_en")
